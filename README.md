3eabed922afcf7a9492407441d5e531938d185ee
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-19 15:14:44 +0900

    ok

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # express-content-type
-content-type 無しでPOSTって受け取れるのか実験
+
+content-type 無しで POST って受け取れるのか実験
+
+## tl;dr
+
+`"Content-Type": "application/json"` がないと body を parse できない。
+body-parser がよし何やってくれるとかではなかった。
+
+## dev
+
+```
+npm i
+
+node index.js
+
+open test.html
+```
--- index.js
+++ index.js
@@ -2,8 +2,38 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, access_token"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.post);
+  res.json({ status: "ok" });
 });
 
 app.listen(port, () => {
